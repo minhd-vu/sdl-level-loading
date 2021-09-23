227943b9e5587f801cb870fc39f819a1d642f443
Minh Vu <minhd_vu@yahoo.com>
2021-09-22 23:57:29 -0400

    updated indentation

--- MyAgent.h
+++ MyAgent.h
@@ -5,154 +5,154 @@
 
 namespace GMUCS425
 {
-  //this defines the transform of the agent
-  class MyAgent
-  {
-  public:
-    //movable: true if this agent can move
-    //collision: true if this agent can collision with other agents
-    MyAgent(bool movable = true, bool collision = true)
-    {
-      x = y = degree = 0;
-      scale = 1;
-      visible = true;
-      sprite = NULL;
-      this->movable = movable;
-      this->collision = collision;
-    }
+	//this defines the transform of the agent
+	class MyAgent
+	{
+	public:
+		//movable: true if this agent can move
+		//collision: true if this agent can collision with other agents
+		MyAgent(bool movable = true, bool collision = true)
+		{
+			x = y = degree = 0;
+			scale = 1;
+			visible = true;
+			sprite = NULL;
+			this->movable = movable;
+			this->collision = collision;
+		}
 
-    //react to the events
-    virtual void handle_event(SDL_Event &e);
+		//react to the events
+		virtual void handle_event(SDL_Event &e);
 
-    //update this agent's motion, looks, sound, etc
-    virtual void update();
+		//update this agent's motion, looks, sound, etc
+		virtual void update();
 
-    //render this agent
-    virtual void display();
+		//render this agent
+		virtual void display();
 
-    //show HUD (heads-up display) or status bar
-    virtual void draw_HUD();
+		//show HUD (heads-up display) or status bar
+		virtual void draw_HUD();
 
-    //transforms
-    void rotate(float degree) { this->degree += degree; }
-    void rotateTo(float degree) { this->degree = degree; }
+		//transforms
+		void rotate(float degree) { this->degree += degree; }
+		void rotateTo(float degree) { this->degree = degree; }
 
-    void tranlate(float x, float y)
-    {
-      this->x += x;
-      this->y += y;
-    }
-    void tranlateTo(float x, float y)
-    {
-      this->x = x;
-      this->y = y;
-    }
+		void tranlate(float x, float y)
+		{
+			this->x += x;
+			this->y += y;
+		}
+		void tranlateTo(float x, float y)
+		{
+			this->x = x;
+			this->y = y;
+		}
 
-    void scaleTo(float s) { this->scale = s; }
+		void scaleTo(float s) { this->scale = s; }
 
-    //display
-    void show() { visible = true; }
-    void hide() { visible = false; }
+		//display
+		void show() { visible = true; }
+		void hide() { visible = false; }
 
-    //sprite, aka costume
-    void setSprite(MySprite *sprite) { this->sprite = sprite; }
+		//sprite, aka costume
+		void setSprite(MySprite *sprite) { this->sprite = sprite; }
 
-    //motion/animation
-    //void glide(float x, float y, float seconds);
+		//motion/animation
+		//void glide(float x, float y, float seconds);
 
-    bool collide(MyAgent *other);
+		bool collide(MyAgent *other);
 
-    //ACCESS METHODS
-    bool is_movable() const { return movable; }
-    bool is_visible() const { return visible; }
+		//ACCESS METHODS
+		bool is_movable() const { return movable; }
+		bool is_visible() const { return visible; }
 
-    float getX() const { return x; }
-    float getY() const { return y; }
-    float getAngle() const { return degree; }
-    float getScale() const { return scale; }
+		float getX() const { return x; }
+		float getY() const { return y; }
+		float getAngle() const { return degree; }
+		float getScale() const { return scale; }
 
-  protected:
-    void draw_bounding_box();
+	protected:
+		void draw_bounding_box();
 
-    //current position and orientations
-    float x, y;
-    float degree; //rotation
-    float scale;
+		//current position and orientations
+		float x, y;
+		float degree; //rotation
+		float scale;
 
-    bool visible;
-    bool movable;
-    bool collision;
+		bool visible;
+		bool movable;
+		bool collision;
 
-    MySprite *sprite; //current sprite
-                      //it is possible that you can have more than one sprites
-                      //vector<MySprite *> sprites; //the sprites
-  };
+		MySprite *sprite; //current sprite
+						  //it is possible that you can have more than one sprites
+						  //vector<MySprite *> sprites; //the sprites
+	};
 
-  class MyZombieAgent : public MyAgent
-  {
-  public:
-    MyZombieAgent(bool movable = true, bool collision = true)
-        : MyAgent(movable, collision)
-    {
-      orig_x = INT_MAX;
-      left = true;
-      collide_with = NULL;
-    }
-    virtual void update();
-    virtual void display();
-    virtual void handle_event(SDL_Event &e);
+	class MyZombieAgent : public MyAgent
+	{
+	public:
+		MyZombieAgent(bool movable = true, bool collision = true)
+			: MyAgent(movable, collision)
+		{
+			orig_x = INT_MAX;
+			left = true;
+			collide_with = NULL;
+		}
+		virtual void update();
+		virtual void display();
+		virtual void handle_event(SDL_Event &e);
 
-  private:
-    int orig_x;
-    bool left;
-    MyAgent *collide_with;
-    int collision_free_timer = 10;
-  };
+	private:
+		int orig_x;
+		bool left;
+		MyAgent *collide_with;
+		int collision_free_timer = 10;
+	};
 
-  class MyChickenAgent : public MyAgent
-  {
-  public:
-    MyChickenAgent(bool movable = true, bool collision = true) : MyAgent(movable, collision)
-    {
-      radius = FLT_MAX;
-      center_x = center_y = INT_MAX;
-      ccw = false;
-      collide_with = NULL;
-    }
-    virtual void update();
-    virtual void display();
-    virtual void handle_event(SDL_Event &e);
+	class MyChickenAgent : public MyAgent
+	{
+	public:
+		MyChickenAgent(bool movable = true, bool collision = true) : MyAgent(movable, collision)
+		{
+			radius = FLT_MAX;
+			center_x = center_y = INT_MAX;
+			ccw = false;
+			collide_with = NULL;
+		}
+		virtual void update();
+		virtual void display();
+		virtual void handle_event(SDL_Event &e);
 
-  private:
-    float radius;
-    int center_x, center_y;
-    bool ccw;
-    MyAgent *collide_with;
-    int collision_free_timer = 10;
-  };
+	private:
+		float radius;
+		int center_x, center_y;
+		bool ccw;
+		MyAgent *collide_with;
+		int collision_free_timer = 10;
+	};
 
-  class MyDragonAgent : public MyAgent
-  {
-  public:
-    MyDragonAgent(bool movable = true, bool collision = true) : MyAgent(movable, collision)
-    {
-      health = 100;
-      dx = dy = 0;
-      collide_with = NULL;
-      left = false;
-    }
-    virtual void handle_event(SDL_Event &e);
-    virtual void update();
-    virtual void draw_HUD();
-    //render this agent
-    virtual void display();
-    int getHealth();
+	class MyDragonAgent : public MyAgent
+	{
+	public:
+		MyDragonAgent(bool movable = true, bool collision = true) : MyAgent(movable, collision)
+		{
+			health = 100;
+			dx = dy = 0;
+			collide_with = NULL;
+			left = false;
+		}
+		virtual void handle_event(SDL_Event &e);
+		virtual void update();
+		virtual void draw_HUD();
+		//render this agent
+		virtual void display();
+		int getHealth();
 
-  private:
-    int health;
-    int dx, dy;
-    bool left;
-    MyAgent *collide_with;
-  };
+	private:
+		int health;
+		int dx, dy;
+		bool left;
+		MyAgent *collide_with;
+	};
 
 } //end namespace
